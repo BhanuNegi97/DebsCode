52fdc00545f8ecf59bfc8fb7db1c77fd51632f43
Bhanu Pratap Singh <bhanusingh@ciqindia.com>
2018-07-10 20:43:04 +0530

    new commit

---
+++ APM-Final V6/src/app/business/business.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pm-business',
+  templateUrl: './business.component.html',
+  styleUrls: ['./business.component.css']
+})
+export class BusinessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
